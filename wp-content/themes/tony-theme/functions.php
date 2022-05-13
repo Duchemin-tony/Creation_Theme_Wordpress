0ca62d06aeb17e7c1fd44c46163a9c42cedb0cd9
Tony-Ismail <duchemin.tony.dev@gmail.com>
2022-05-13 15:40:29 +0200

    Gestion du menu Wordpress

--- wp-content/themes/tony-theme/functions.php
+++ wp-content/themes/tony-theme/functions.php
@@ -3,6 +3,9 @@
 function tony_supports () {
     add_theme_support('title-tag');
     add_theme_support('post-thumbails');
+    add_theme_support('menus');
+    register_nav_menu('header', 'En tête du menu');
+    register_nav_menu('footer', 'Pied de page');
 }
 
 function tony_register_assets(){
@@ -15,9 +18,16 @@ function tony_register_assets(){
     wp_enqueue_script('bootstrap');
 }
 
-
-
-
+function tony_theme_class($classes){
+    $classes[] = 'nav-item';
+    return $classes;
+}
+function tony_theme_link_class($attrs){
+    $attrs['class'] = 'nav-link';
+    return $attrs;
+}
 
  add_action('after_setup_theme', 'tony_supports');
  add_action('wp_enqueue_scripts', 'tony_register_assets');
+ add_filter('nav_menu_css_class', 'tony_theme_class');
+ add_filter('nav_menu_link_attributes', 'tony_theme_link_class');
