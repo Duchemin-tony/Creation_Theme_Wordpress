adbd5647b333521ab010d481629b081f5d3b61c4
Tony-Ismail <duchemin.tony.dev@gmail.com>
2022-05-13 12:48:51 +0200

    Hiérarchie des template

---
+++ wp-content/themes/tony-theme/404.php
@@ -0,0 +1,10 @@
+<?php get_header() ?>
+
+<h1>Erreur 404</h1>
+<p>Cette page est introuvable...</p>
+<a href="http://localhost:8888/Creation_Theme_Wordpress/">Retour à l'accueil</a>
+
+
+
+
+<?php get_footer() ?>
